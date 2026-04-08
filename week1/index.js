7a7d16828341d3deaae84107e9bb2095d01af543
w62826 <baemg2007@gmail.com>
2026-04-08 09:58:13 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/food", (req, res) => {
+    res.send("<h1>Wanna Eat?</h1>");
+});
+
+app.get("/burger", (req, res) => {
+    res.send("<h1>Not Healthy!</h1>");
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8080!');
+});
